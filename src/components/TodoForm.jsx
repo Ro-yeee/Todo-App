7259a358ee65b9ab0739d2dc6d5396463da2d594
Roy Joseph <royjoseph.mec@gmail.com>
2023-07-07 19:35:03 +0530

    Changed the styling of radio buttons and also cleared their status on form submit

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,17 +3,18 @@ import { useState } from 'react'
 
 function TodoForm({addTodo}) {
   const [todo,setTodo] = useState("")
-  const [priority,setPriority] = useState(0)
+  const [priority,setPriority] = useState({status:false,val:null})
   return (
     <form onSubmit={(e) =>{
       e.preventDefault()
-      addTodo(todo,priority)
+      addTodo(todo,priority.val)
       setTodo("")
+      setPriority({status:false,val:null})
     }} className='TodoForm'>
       <div className='radioBtnsContainer'>
-        <input onChange={() => setPriority(0)} type="radio" name="priority" id="low" className='radio' required></input>
-        <input onChange={() => setPriority(1)} type="radio" name="priority" id="medium" className='radio' required></input>
-        <input onChange={() => setPriority(2)} type="radio" name="priority" id="high" className='radio' required></input>
+        <input checked={priority.status} onChange={(e) => setPriority({status:e.target.checked.id,val:0})} type="radio" name="priority" className='radio0' required></input>
+        <input checked={priority.status} onChange={(e) => setPriority({status:e.target.checked.id,val:1})} type="radio" name="priority" className='radio1' required></input>
+        <input checked={priority.status} onChange={(e) => setPriority({status:e.target.checked.id,val:2})} type="radio" name="priority" className='radio2' required></input>
       </div>
       <input value={todo} onChange={(e) => {setTodo(e.target.value)}} className='todo-input' type="text" placeholder='What is the task today?' required/>
       <button className='submit-btn' type='submit'>Add Task</button>
