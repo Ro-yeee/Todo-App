225099675b560be8708af5359cc43c4114cab193
Roy Joseph <royjoseph.mec@gmail.com>
2023-07-08 15:18:33 +0530

    priority button now retains their previous value when we go into edit mode

--- src/components/EditTodoForm.jsx
+++ src/components/EditTodoForm.jsx
@@ -3,19 +3,19 @@ import { useState } from 'react'
 
 function EditTodoForm({editTodo,element}) {
   const [todo,setTodo] = useState(element.task)
-  const [priority,setPriority] = useState(null)
+  const [prio,setPriority] = useState(element.priority)
 
   return (
     <form onSubmit={(e) =>{
       e.preventDefault()
-      editTodo(todo,priority,element.id)
+      editTodo(todo,prio,element.id)
       setTodo("")
       setPriority({status:false,val:null})
     }} className='TodoForm'>
       <div className='radioBtnsContainer'>
-        <input  onChange={(e) => setPriority("0")} type="radio" name="priority" className='radio0' required></input>
-        <input  onChange={(e) => setPriority("1")} type="radio" name="priority" className='radio1' required></input>
-        <input  onChange={(e) => setPriority("2")} type="radio" name="priority" className='radio2' required></input>
+        <input checked = {prio === '0'} onChange={(e) => setPriority("0")} type="radio" name="priority" className='radio0' required></input>
+        <input checked = {prio === "1"} onChange={(e) => setPriority("1")} type="radio" name="priority" className='radio1' required></input>
+        <input checked = {prio === "2"} onChange={(e) => setPriority("2")} type="radio" name="priority" className='radio2' required></input>
       </div>
       <input value={todo} onChange={(e) => {setTodo(e.target.value)}} className='todo-input' type="text" placeholder='What is the updated task?' required/>
       <button className='submit-btn' type='submit'>Update Task</button>
--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -3,18 +3,18 @@ import { useState } from 'react'
 
 function TodoForm({addTodo}) {
   const [todo,setTodo] = useState("")
-  const [priority,setPriority] = useState({status:false,val:null})
+  const [priority,setPriority] = useState(null)
   return (
     <form onSubmit={(e) =>{
       e.preventDefault()
-      addTodo(todo,priority.val)
+      addTodo(todo,priority)
       setTodo("")
-      setPriority({status:false,val:null})
+      setPriority(null)
     }} className='TodoForm'>
       <div className='radioBtnsContainer'>
-        <input checked={priority.status} onChange={(e) => setPriority({status:e.checked,val:"0"})} type="radio" name="priority" className='radio0' required></input>
-        <input checked={priority.status} onChange={(e) => setPriority({status:e.checked,val:"1"})} type="radio" name="priority" className='radio1' required></input>
-        <input checked={priority.status} onChange={(e) => setPriority({status:e.checked,val:"2"})} type="radio" name="priority" className='radio2' required></input>
+        <input checked={priority === "0"} onChange={(e) => setPriority("0")} type="radio" name="priority" className='radio0' required></input>
+        <input checked={priority === "1"} onChange={(e) => setPriority("1")} type="radio" name="priority" className='radio1' required></input>
+        <input checked={priority === "2"} onChange={(e) => setPriority("2")} type="radio" name="priority" className='radio2' required></input>
       </div>
       <input value={todo} onChange={(e) => {setTodo(e.target.value)}} className='todo-input' type="text" placeholder='What is the task today?' required/>
       <button className='submit-btn' type='submit'>Add Task</button>
